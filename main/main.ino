f3e4d2d162377c102cd65b156eec4513a3aa62f5
piotr <piotr@infordb.com>
2024-04-20 16:58:18 +0200

    Blink without delay, Serial.read, delay 1000 => 50

--- main/main.ino
+++ main/main.ino
@@ -1,44 +1,81 @@
-#include <ArduinoJson.h>
-#define LED_BUILTIN 2
+  #include <Wire.h>
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_LEDBackpack.h>
+  #include <ArduinoJson.h>
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-}
+  #define LED_BUILTIN 2
 
-void loop() {
-  listenToCommands();
-  
-}
+  //Blink
+  unsigned long previousMillis = 0;
+  const long interval = 200;
+  bool isBlinking = false;
 
-void listenToCommands(){
-  if(Serial.available() <= 0){ return; }
+  //Matrix
+  Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
 
-  String data = Serial.readString();
-  blinkLED();
+  void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    matrix.begin(0x70);
+    matrix.setBrightness(15);
 
-  StaticJsonDocument<200> json;
-  DeserializationError error = deserializeJson(json, data);
-
-  if (error) {
-    Serial.println("FAIL");
-    return;
+    Serial.begin(115200);
+    Serial.setTimeout(50);
   }
 
-  String command = json["command"];
-  if(command == "INIT"){
-    init();
+  void loop() {
+    listenToCommands();
+    handleBlink();
   }
 
-}
+  void listenToCommands(){
+    if(Serial.available() <= 0){ return; }
 
-void init(){
-  Serial.println("OK");
-}
+    String data = Serial.readString();
 
-void blinkLED() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(200);
-}
+    StaticJsonDocument<200> json;
+    DeserializationError error = deserializeJson(json, data);
+
+    if (error) {
+      Serial.println("FAIL");
+      return;
+    }
+
+    String command = json["command"];
+    if(command == "INIT"){
+      init();
+    }
+    else if(command == "VISUALIZE"){
+      visualize(json["image"]);
+    }
+
+    Serial.println("OK");
+    blink();
+  }
+
+  void init(){
+    Serial.println("OK");
+  }
+  void visualize(String image){
+    Serial.println(image);
+
+    // matrix.clear();
+    // matrix.drawPixel(4, 4, LED_ON);
+    // matrix.drawPixel(5, 5, LED_ON);
+    // matrix.writeDisplay();
+  }
+
+  void blink() {
+    isBlinking = true;
+    previousMillis = millis();
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
+  void handleBlink() {
+    if (isBlinking) {
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) {
+        digitalWrite(LED_BUILTIN, LOW);
+        isBlinking = false;
+      }
+    }
+  }
